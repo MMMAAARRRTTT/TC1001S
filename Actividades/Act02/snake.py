19aaa155c02817b4b28ebe04a59195f863d56af0
Marco Montoya <mctulio.2@gmail.com>
2022-05-12 17:17:18 -0700

    Se terminó el codigo snake.py

--- Actividades/Act02/snake.py
+++ Actividades/Act02/snake.py
@@ -19,7 +19,7 @@ snake = [vector(10, 0)]
 aim = vector(0, -10)
 
 """Definimos los colores"""
-colors = ["blue", "green", "purple", "black", "yellow"]
+colors = ["blue", "green", "purple", "black", "orange"]
 bodyColor = colors[random.randint(0,4)]
 foodColor = colors[random.randint(0,4)]
 
@@ -43,41 +43,3 @@ def move():
     """Move snake forward one segment."""
     head = snake[-1].copy()
     head.move(aim)
-
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
-        update()
-        return
-
-    snake.append(head)
-
-    if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
-    else:
-        snake.pop(0)
-
-    clear()
-
-    for body in snake:
-        square(body.x, body.y, 9, bodyColor)
-
-    square(food.x, food.y, 9, foodColor)
-    update()
-    ontimer(move, 100)
-
-
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
-move()
-done()
-
-
-"jaja"
